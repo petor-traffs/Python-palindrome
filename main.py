1c05bddf09992cb39ae0748e06a3adfe7a83ebda
Pedro Rodríguez <63085366+petor-traffs@users.noreply.github.com>
2020-10-12 12:59:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+
+class Solution:
+
+    def __init__(self):
+        self.stack = []
+        self.queue = []
+    def pushCharacter(self, ch):
+        self.stack.append(ch)
+
+    def enqueueCharacter(self, ch):
+        self.queue.append(ch)
+
+    def popCharacter(self):
+        temporalVariable = self.stack.pop()
+        return temporalVariable
+
+    def dequeueCharacter(self):
+        temporalVariable2 = self.queue.pop(0)
+        return temporalVariable2
+
+
+
+p9 = Solution("hello")
